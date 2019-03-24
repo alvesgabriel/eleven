06fb9d46fecff00531d69864b6d7bd2660d12afa
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2019-03-24 20:35:59 -0300

    Created draft land page
    close #8

--- eleven/base/views.py
+++ eleven/base/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    # return render(request, 'Hello World')
-    return HttpResponse('Hello World')
+    return render(request, 'base/index.html')
+    # return HttpResponse('Hello World')
